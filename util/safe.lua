eab49bb05bc2a7462438d21ef22b2381970ccb61
Guilherme Dantas <guidanoli@hotmail.com>
2023-04-19 00:05:46 -0300

    fix: make `util.safe` only warn of nil-reads

--- util/safe.lua
+++ util/safe.lua
@@ -1,6 +1,6 @@
 setmetatable(_G, {
     __index = function (t, k)
-        error(string.format('unset global variable %s', k))
+        warn(string.format('unset global variable %s', k))
     end,
     __newindex = function (t, k, v)
         warn(string.format('set global variable %s to %s', k, v))

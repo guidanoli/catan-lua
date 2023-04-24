e5512f1688aca126ebdbcd77f040fed33e04b2e6
Guilherme Dantas <guidanoli@hotmail.com>
2023-04-24 13:25:27 -0300

    test: print game table if validation fails

--- test/catan/game.lua
+++ test/catan/game.lua
@@ -1,5 +1,12 @@
 require "util.safe"
+local serpent = require "serpent"
 
 local Game = require "catan.logic.game"
+local GameSchema = require "catan.logic.schema"
 
 local g = Game:new()
+local ok, err = pcall(function() GameSchema:validate(g) end)
+if not ok then
+    print(serpent.block(g))
+    error(err)
+end

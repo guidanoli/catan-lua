8abc904c76f10f2ad4816740e3f77e1cf18bb833
Guilherme Dantas <guidanoli@hotmail.com>
2023-04-25 15:29:15 -0300

    refactor: `validate` function in `test.catan.game`

--- test/catan/game.lua
+++ test/catan/game.lua
@@ -4,9 +4,12 @@ local serpent = require "serpent"
 local Game = require "catan.logic.game"
 local GameSchema = require "catan.logic.schema"
 
-local g = Game:new()
-local ok, err = pcall(function() GameSchema:validate(g) end)
-if not ok then
-    print(serpent.block(g))
-    error(err)
+local function validate(g)
+    local ok, err = pcall(function() GameSchema:validate(g) end)
+    if not ok then
+        print(serpent.block(g))
+        error(err)
+    end
 end
+
+validate(Game:new())

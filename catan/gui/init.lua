217a7e6866b2c5361e2d6562366b276a7866fa8f
Guilherme Dantas <guidanoli@hotmail.com>
2023-05-05 22:40:42 -0300

    feat(gui): render harbors (not ships yet)

--- catan/gui/init.lua
+++ catan/gui/init.lua
@@ -1,5 +1,7 @@
 local Game = require "catan.logic.game"
 local FaceMap = require "catan.logic.facemap"
+local VertexMap = require "catan.logic.vertexmap"
+local Grid = require "catan.logic.grid"
 
 -- Lua 5.1 compat
 table.unpack = table.unpack or unpack
@@ -27,8 +29,8 @@ catan.debug = os.getenv "DEBUG" ~= nil
 --
 -- 1) Sea background - OK
 -- 2) Hex tiles - OK
--- 3) Harbor ships
--- 4) Harbors
+-- 3) Harbors - OK
+-- 4) Harbor ships
 -- 5) Numbers
 -- 6) Robber
 -- 7) Roads
@@ -92,8 +94,79 @@ function catan:getFaceCenter (q, r)
     return x, y
 end
 
+function catan:getVertexPos (q, r, v)
+    local x, y = self:getFaceCenter(q, r)
+    if v == 'N' then
+        y = y - self.HEXSIZE
+    else
+        assert(v == 'S')
+        y = y + self.HEXSIZE
+    end
+    return x, y
+end
+
+-- Convert degrees to radians (x pi/180) and
+-- corrects the direction of increasing angles (x -1)
+function catan:deg2rad (a)
+    return - math.pi * a / 180.
+end
+
+-- Returns angles for north-vertex and south-vertex
+-- in degrees (and assuming anti-clockwise direction)
+function catan:harbourAnglesFromOrientation (o)
+    if o == 'NE' then
+        return 30, 90
+    elseif o == 'NW' then
+        return 150, 90
+    elseif o == 'W' then
+        return 150, -150
+    elseif o == 'SW' then
+        return -90, -150
+    elseif o == 'SE' then
+        return -90, -30
+    else
+        assert(o == 'E')
+        return 30, -30
+    end
+end
+
+function catan:getHarbourAngles (vertex1, vertex2)
+    local edge = Grid:edgeInBetween(vertex1, vertex2)
+
+    -- Here, we find the neighbouring face with a hex
+    local face
+    for i, joinedFace in ipairs(Grid:joins(edge)) do
+        if FaceMap:get(self.game.hexmap, joinedFace) then
+            face = joinedFace
+        end
+    end
+    assert(face ~= nil, "no neighbouring hex")
+
+    -- Now we calculate the angle of each harbor
+    -- depending on the orientation of the edge
+    -- in the neighbouring face with a hex
+    local o = Grid:edgeOrientationInFace(face, edge)
+    local r1, r2 = self:harbourAnglesFromOrientation(o)
+
+    -- Above, we assume vertex1 is a north-vertex and
+    -- vertex2 is a south-vertex. If this is not true,
+    -- we swap r1 and r2.
+    if vertex1.v ~= 'N' then
+        assert(vertex1.v == 'S')
+        r1, r2 = r2, r1
+    end
+
+    -- Now, we convert degrees to radians
+    r1 = self:deg2rad(r1)
+    r2 = self:deg2rad(r2)
+
+    return r1, r2
+end
+
 function catan:updateSprites ()
     self.sprites = {}
+
+    -- Hexes
     FaceMap:iter(self.game.hexmap, function (q, r, hex)
         local x, y = self:getFaceCenter(q, r)
         local img = assert(self.images.hex[hex], "missing hex sprite")
@@ -102,6 +175,27 @@ function catan:updateSprites ()
         local s = self.HEXSIZE / (h / 2)
         table.insert(self.sprites, {img, x, y, nil, s, nil, ox, oy})
     end)
+
+    -- Harbors
+    do
+        local visited = {}
+        local harborImg = self.images.harbor
+        local oy = harborImg:getHeight() / 2
+        VertexMap:iter(self.game.harbormap, function (q1, r1, v1, harbor)
+            local vertex1 = Grid:vertex(q1, r1, v1)
+            VertexMap:set(visited, vertex1, true)
+            local adjvertices = Grid:adjacentVertices(vertex1)
+            for _, vertex2 in ipairs(adjvertices) do
+                if VertexMap:get(visited, vertex2) then
+                    local x1, y1 = self:getVertexPos(q1, r1, v1)
+                    local x2, y2 = self:getVertexPos(Grid:unpack(vertex2))
+                    local a1, a2 = self:getHarbourAngles(vertex1, vertex2)
+                    table.insert(self.sprites, {harborImg, x1, y1, a1, nil, nil, nil, oy})
+                    table.insert(self.sprites, {harborImg, x2, y2, a2, nil, nil, nil, oy})
+                end
+            end
+        end)
+    end
 end
 
 function catan:update (dt)

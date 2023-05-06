d24f0479ebf2481de7ecf127b9381951b753de03
Guilherme Dantas <guidanoli@hotmail.com>
2023-05-05 22:39:59 -0300

    feat(logic): add functions to Grid for working with edges

--- catan/logic/grid.lua
+++ catan/logic/grid.lua
@@ -107,4 +107,61 @@ function Grid:adjacentVertices (vertex)
     end
 end
 
+function Grid:joins (edge)
+    local q, r, e = self:unpack(edge)
+    if e == 'NE' then
+        return {
+            self:face(q+1, r-1),
+            self:face(q, r),
+        }
+    elseif e == 'NW' then
+        return {
+            self:face(q, r),
+            self:face(q, r-1),
+        }
+    else
+        assert(e == 'W')
+        return {
+            self:face(q, r),
+            self:face(q-1, r),
+        }
+    end
+end
+
+function Grid:edgeInBetween (vertex1, vertex2)
+    local edges1 = self:protrudingEdges(vertex1)
+    local edges2 = self:protrudingEdges(vertex2)
+    for i, edge1 in ipairs(edges1) do
+        for j, edge2 in ipairs(edges2) do
+            if self:edgeEq(edge1, edge2) then
+                return edge1
+            end
+        end
+    end
+    error"no edge in-between"
+end
+
+function Grid:edgeOrientationInFace (face, edge)
+    local q, r, e = self:unpack(edge)
+    if q == face.q and r == face.r then
+        assert(e == 'NE' or e == 'NW' or e == 'W')
+        return e
+    else
+        if e == 'NE' then
+            assert(q == face.q - 1)
+            assert(r == face.r + 1)
+            return 'SW'
+        elseif e == 'NW' then
+            assert(q == face.q)
+            assert(r == face.r + 1)
+            return 'SE'
+        else
+            assert(q == face.q + 1)
+            assert(r == face.r)
+            assert(e == 'W')
+            return 'E'
+        end
+    end
+end
+
 return Grid

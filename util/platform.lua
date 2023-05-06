b897f7b177166d7bce145d78a0023564d2f130d0
Guilherme Dantas <guidanoli@hotmail.com>
2023-05-05 22:57:00 -0300

    feat(util): new module `platform`

---
+++ util/platform.lua
@@ -0,0 +1,12 @@
+---
+-- Defines platform-specific constants
+--
+-- @module util.platform
+
+local platform = {}
+
+---
+-- Path separator
+platform.PATH_SEPARATOR = package.config:sub(1, 1)
+
+return platform

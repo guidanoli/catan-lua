74d1329bc7740e3468fc868adc4f0ab6a4f04a1d
Guilherme Dantas <guidanoli@hotmail.com>
2023-05-12 00:05:45 -0300

    feat(util): add `math.type` to `util.compat`

--- util/compat.lua
+++ util/compat.lua
@@ -4,7 +4,22 @@
 -- Defines the following variables:
 --
 -- * `table.unpack` (from `unpack`)
+-- * `math.type`
 --
 -- @module util.compat
 
 table.unpack = table.unpack or unpack
+
+if math.type == nil then
+    function math.type(n)
+        if type(n) == "number" then
+            if math.floor(n) == n then
+                return "integer"
+            else
+                return "float"
+            end
+        else
+            error("not a number", 2)
+        end
+    end
+end

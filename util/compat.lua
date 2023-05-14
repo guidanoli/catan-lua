d99fde469177932a6cdc24dee5d6d6518afc710f
Guilherme Dantas <guidanoli@hotmail.com>
2023-05-14 19:50:15 -0300

    refactor(util): remove `math.type`

--- util/compat.lua
+++ util/compat.lua
@@ -4,22 +4,7 @@
 -- Defines the following variables:
 --
 -- * `table.unpack` (from `unpack`)
--- * `math.type`
 --
 -- @module util.compat
 
 table.unpack = table.unpack or unpack
-
-if math.type == nil then
-    function math.type(n)
-        if type(n) == "number" then
-            if math.floor(n) == n then
-                return "integer"
-            else
-                return "float"
-            end
-        else
-            error("not a number", 2)
-        end
-    end
-end

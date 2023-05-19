1f4fcc26611931e4566017aff41e49d95e1d1026
Guilherme Dantas <guidanoli@hotmail.com>
2023-05-19 12:27:28 -0300

    feat(gui): add `Layer:addSpriteLine`

--- catan/gui/layer.lua
+++ catan/gui/layer.lua
@@ -27,6 +27,59 @@ function Layer:addSprite (t)
     return sprite
 end
 
+---
+-- Add a horizontal line of sprites
+-- @tparam {Drawable,...,x=number,y=number,sep=number,xalign=string,yalign=string}
+--         line an array of `Drawable` inputs and some paramaters about positioning
+function Layer:addSpriteLine (t)
+    local x = t.x or 0
+    local y = t.y or 0
+    local sep = t.sep or 0
+    local sx = t.sx or 1
+    local sy = t.sy or sx
+    local xalign = t.xalign or 'left'
+    local yalign = t.yalign or 'top'
+
+    local w = 0
+    local first = true
+    for i, drawable in ipairs(t) do
+        w = w + drawable:getWidth() * sx
+        if not first then
+            w = w + sep
+        end
+        first = false
+    end
+
+    local maxh = 0
+    for i, drawable in ipairs(t) do
+        local h = drawable:getHeight()
+        if h > maxh then maxh = h end
+    end
+    maxh = maxh * sy
+
+    if xalign == 'center' then
+        x = x - w / 2
+    elseif xalign == 'right' then
+        x = x - w
+    end
+
+    if yalign == 'center' then
+        y = y - maxh / 2
+    elseif yalign == 'bottom' then
+        y = y - maxh
+    end
+
+    local sprites = {}
+
+    for i, drawable in ipairs(t) do
+        local sprite = self:addSprite{drawable, x=x, y=y, sx=sx, sy=sy}
+        table.insert(sprites, sprite)
+        x = x + sep + sprite:getWidth()
+    end
+
+    return sprites
+end
+
 function Layer:iterSprites (f)
     for i, sprite in ipairs(self) do
         local ret = f(sprite)

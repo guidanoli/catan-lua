469feb19e1eed6e1db4316619d68f607b9f0f829
Guilherme Dantas <guidanoli@hotmail.com>
2023-05-19 12:27:12 -0300

    docs(gui): document `catan.gui.layer`

--- catan/gui/layer.lua
+++ catan/gui/layer.lua
@@ -1,13 +1,26 @@
+---
+-- Array of sprites
+-- @module catan.gui.layer
+
 local Class = require "util.class"
 
 local Sprite = require "catan.gui.sprite"
 
+---
+-- @type Layer
 local Layer = Class "Layer"
 
+---
+-- Create an empty layer
+-- @treturn Layer the newly-created layer
 function Layer:new ()
     return Layer:__new{}
 end
 
+---
+-- Add sprite to layer
+-- @tparam {Sprite,...} t input to `Sprite`
+-- @treturn Sprite the newly-created sprite
 function Layer:addSprite (t)
     local sprite = Sprite.new(t)
     table.insert(self, sprite)

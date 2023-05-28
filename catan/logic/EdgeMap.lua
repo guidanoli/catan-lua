fcaac78f0c0dfad9aeeee3fbc19de5378e7ea591
Guilherme Dantas <guidanoli@hotmail.com>
2023-05-28 20:55:56 -0300

    docs(logic): document `EdgeMap`

--- catan/logic/EdgeMap.lua
+++ catan/logic/EdgeMap.lua
@@ -1,3 +1,8 @@
+---
+-- An associative array with hexagonal grid edges as keys.
+--
+-- @classmod catan.logic.EdgeMap
+
 local Class = require "util.class"
 
 local Grid = require "catan.logic.grid"
@@ -7,10 +12,23 @@ local Edge = CatanSchema.Edge
 
 local EdgeMap = Class "EdgeMap"
 
+---
+-- Create an empty edge map.
+-- @treturn catan.logic.EdgeMap an empty edge map
 function EdgeMap:new ()
     return self:__new{}
 end
 
+---
+-- Get the object associated with `edge`.
+-- @tparam {q=number,r=number,e='NW'|'W'|'NE'} edge
+-- @return the object associated with `edge`
+-- @usage
+-- local edgemap = EdgeMap:new()
+-- local edge = {q=1, r=0, e='W'}
+-- print(edgemap:get(edge)) --> nil
+-- print(edgemap:set(edge, 'hah'))
+-- print(edgemap:get(edge)) --> hah
 function EdgeMap:get (edge)
     assert(Edge:isValid(edge))
     local q, r, e = Grid:unpack(edge)
@@ -23,6 +41,18 @@ function EdgeMap:get (edge)
     end
 end
 
+---
+-- Associate `edge` with some object.
+-- You can also remove any association with `edge`
+-- by passing the value `nil` as parameter `o`.
+-- @tparam {q=number,r=number,e='NW'|'W'|'NE'} edge
+-- @param o the object to associate `edge` with
+-- @usage
+-- local edgemap = EdgeMap:new()
+-- local edge = {q=1, r=0, e='W'}
+-- print(edgemap:get(edge)) --> nil
+-- edgemap:set(edge, 'hah')
+-- print(edgemap:get(edge)) --> hah
 function EdgeMap:set (edge, o)
     assert(Edge:isValid(edge))
     local q, r, e = Grid:unpack(edge)
@@ -39,6 +69,26 @@ function EdgeMap:set (edge, o)
     rawset(mapqr, e, o)
 end
 
+---
+-- Iterate through all the associations.
+-- @tparam function f the iterator that will be called
+-- for each association, receiving as parameter the
+-- values `q`, `r`, `e` and `o`, where `q`, `r` and `e` uniquely
+-- identify the edge and `o` is the value associated with
+-- such edge. If this function returns a value different
+-- from `nil` or `false`, iteration is interrupted and this
+-- value is returned immediately.
+-- @usage
+-- local haystack = EdgeMap:new()
+-- -- ...
+-- local edge = haystack:iter(function (q, r, e, o)
+--   if o == 'needle' then
+--     return {q = q, r = r, e = e}
+--   end
+-- end)
+-- if edge ~= nil then
+--   print(haystack:get(edge)) --> needle
+-- end
 function EdgeMap:iter (f)
     for q, mapq in pairs(self) do
         for r, mapqr in pairs(mapq) do

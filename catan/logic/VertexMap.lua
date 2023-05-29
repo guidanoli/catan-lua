9d4920216cede7e6a113219808f8979188329129
Guilherme Dantas <guidanoli@hotmail.com>
2023-05-28 21:24:17 -0300

    docs(logic): document `VertexMap`

--- catan/logic/VertexMap.lua
+++ catan/logic/VertexMap.lua
@@ -1,3 +1,8 @@
+---
+-- An associative array with hexagonal grid vertices as keys.
+--
+-- @classmod catan.logic.VertexMap
+
 local Class = require "util.class"
 
 local Grid = require "catan.logic.grid"
@@ -7,10 +12,23 @@ local Vertex = CatanSchema.Vertex
 
 local VertexMap = Class "VertexMap"
 
+---
+-- Create an empty vertex map.
+-- @treturn catan.logic.VertexMap an empty vertex map
 function VertexMap:new ()
     return self:__new{}
 end
 
+---
+-- Get the object associated with `vertex`.
+-- @tparam {q=number,r=number,v='N'|'S'} vertex
+-- @return the object associated with `vertex`
+-- @usage
+-- local vertexmap = VertexMap:new()
+-- local vertex = {q=1, r=0, v='N'}
+-- print(vertexmap:get(vertex)) --> nil
+-- print(vertexmap:set(vertex, 'hah'))
+-- print(vertexmap:get(vertex)) --> hah
 function VertexMap:get (vertex)
     assert(Vertex:isValid(vertex))
     local q, r, v = Grid:unpack(vertex)
@@ -23,6 +41,18 @@ function VertexMap:get (vertex)
     end
 end
 
+---
+-- Associate `vertex` with some object.
+-- You can also remove any association with `vertex`
+-- by passing the value `nil` as parameter `o`.
+-- @tparam {q=number,r=number,v='N'|'S'} vertex
+-- @param o the object to associate `vertex` with
+-- @usage
+-- local vertexmap = VertexMap:new()
+-- local vertex = {q=1, r=0, v='N'}
+-- print(vertexmap:get(vertex)) --> nil
+-- vertexmap:set(vertex, 'hah')
+-- print(vertexmap:get(vertex)) --> hah
 function VertexMap:set (vertex, o)
     assert(Vertex:isValid(vertex))
     local q, r, v = Grid:unpack(vertex)
@@ -39,6 +69,26 @@ function VertexMap:set (vertex, o)
     rawset(mapqr, v, o)
 end
 
+---
+-- Iterate through all the associations.
+-- @tparam function f the iterator that will be called
+-- for each association, receiving as parameter the
+-- values `q`, `r`, `v` and `o`, where `q`, `r` and `v` uniquely
+-- identify the vertex and `o` is the value associated with
+-- such vertex. If this function returns a value different
+-- from `nil` or `false`, iteration is interrupted and this
+-- value is returned immediately.
+-- @usage
+-- local haystack = VertexMap:new()
+-- -- ...
+-- local vertex = haystack:iter(function (q, r, v, o)
+--   if o == 'needle' then
+--     return {q = q, r = r, v = v}
+--   end
+-- end)
+-- if vertex ~= nil then
+--   print(haystack:get(vertex)) --> needle
+-- end
 function VertexMap:iter (f)
     for q, mapq in pairs(self) do
         for r, mapqr in pairs(mapq) do

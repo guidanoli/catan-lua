a72b3f3bfab3929e1c4b4acd01e5396017f8a598
Guilherme Dantas <guidanoli@proton.me>
2023-06-27 00:53:47 -0300

    test(logic): invalid input

--- test/catan/logic/game.lua
+++ test/catan/logic/game.lua
@@ -6,7 +6,7 @@ local TableUtils = require "util.table"
 local schema = require "util.schema"
 
 local Game = require "catan.logic.game"
-local Catan = require "catan.logic.schema"
+local Grid = require "catan.logic.grid"
 
 Game:new():validate()
 Game:new{'red', 'blue', 'white'}:validate()
@@ -35,33 +35,54 @@ local stateFiles = {
     "end.lua",
 }
 
+local function loadFrom (stateFile)
+    local path = {"test", "catan", "states", stateFile}
+    local pathStr = table.concat(path, platform.PATH_SEPARATOR)
+    local fp = io.open(pathStr)
+    local str = fp:read"*a"
+    fp:close()
+
+    return assert(Game:deserialize(str))
+end
+
 local function mutate (game)
     game.player = "foobar"
 end
 
 for _, stateFile in ipairs(stateFiles) do
-    local path = {"test", "catan", "states", stateFile}
-    local pathStr = table.concat(path, platform.PATH_SEPARATOR)
-    local fp = io.open(pathStr)
-    local str1 = fp:read"*a"
-    fp:close()
-
-    local game1 = assert(Game:deserialize(str1))
+    local game1 = loadFrom(stateFile)
 
     game1:validate()
 
-    local str2 = game1:serialize()
-    local game2 = assert(Game:deserialize(str2))
+    local str1 = game1:serialize()
+    local game2 = assert(Game:deserialize(str1))
 
     assert(TableUtils:deepEqual(game1, game2, true))
 
     mutate(game2)
 
-    local str3 = game2:serialize()
-    assert(not Game:deserialize(str3))
+    local str2 = game2:serialize()
+    assert(not Game:deserialize(str2))
 end
 
 assert(not Game:deserialize"return {")
 assert(not Game:deserialize"error()")
 assert(not Game:deserialize"return 123")
 assert(not Game:deserialize"return {}")
+
+-- placeInitialSettlement
+
+do
+    local game = Game:new()
+    assert(not game:canPlaceInitialSettlement"foo")
+    assert(not game:canPlaceInitialSettlement{q=777, r=777, v='N'})
+end
+
+-- placeInitialRoad
+
+do
+    local game = Game:new()
+    game:placeInitialSettlement{q=0, r=0, v='N'}
+    assert(not game:canPlaceInitialRoad"foo")
+    assert(not game:canPlaceInitialRoad{q=777, r=777, e='W'})
+end

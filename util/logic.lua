4842addb3a9ee8ba67c7255de3906d38ce4a8342
Guilherme Dantas <guidanoli@hotmail.com>
2023-06-28 17:29:17 -0300

    docs(util): document `util.logic`

--- util/logic.lua
+++ util/logic.lua
@@ -1,13 +1,32 @@
+---
+-- Propositional logic operations.
+--
+-- @module util.logic
+
 local LogicUtils = {}
 
+---
+-- Get truth value of `x`.
+-- @param x
+-- @treturn boolean truth value of `x`
 function LogicUtils:tobool (x)
     return not not x
 end
 
+---
+-- Material implication `a -> b`
+-- @tparam boolean a
+-- @tparam boolean b
+-- @treturn boolean `a -> b`
 function LogicUtils:implies (a, b)
     return not self:tobool(a) or self:tobool(b)
 end
 
+---
+-- Logical equivalence `a <-> b`
+-- @tparam boolean a
+-- @tparam boolean b
+-- @treturn boolean `a <-> b`
 function LogicUtils:iff (a, b)
     local a = self:tobool(a)
     local b = self:tobool(b)
